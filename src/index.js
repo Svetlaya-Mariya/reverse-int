46c595e14dc3bb27c4cc6d3fcdbb5730fecc89d2
Мария Светлая <merry76light@gmail.com>
2021-03-15 00:21:14 +0300

    feat: add revers-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let rever = [];
+  n = Math.abs(n);
+  while (n){
+    let a = n%10;
+    n = (n-a)/10;
+    rever.push(a);
+  }
+    let num = rever.join('');
+    return num;
 }
